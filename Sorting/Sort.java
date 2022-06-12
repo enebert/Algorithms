3920e6898f7afcec5b8bd3de51853a0b95f96d77
enebert <ebert.eric.n@protonmail.com>
2022-06-12 00:27:20 -0400

    Finished Merge Sort

--- Sorting/Sort.java
+++ Sorting/Sort.java
@@ -1,3 +1,4 @@
+import java.util.*;
 import java.lang.Math;
 
 public class Sort{
@@ -32,19 +33,61 @@ public class Sort{
     }
 
     public static void merge_sort(int[] seq){
-        int last = seq.length-1;
-        if (last <= 1) return;
-
-        q = Math.floor((last/2));
-
-        merge(merge_sort(firsHalf), merge_sort(lastHalf));
+        merge_sort_rec(seq, 0, seq.length-1);
     }
 
     public static void merge_sort_mod(int[] seq){
 
     }
 
-    private static int[] merge(int[] s, int[] n){
+    private static void merge_sort_rec(int[] seq, int start, int last){
+        if(last - start <= 0){
+            return;
+        }
 
+        int mid = (start + last) / 2;
+
+        merge_sort_rec(seq, start, mid);
+        merge_sort_rec(seq, mid+1, last);
+        merge(seq, start, mid, last);
+    }
+
+    private static void merge(int[] seq, int start, int mid, int stop){
+        int leftLength;
+        int rightLength;
+
+        int[] tempLeft = Arrays.copyOfRange(seq, start, mid+1);
+        int[] tempRight = Arrays.copyOfRange(seq, mid+1, stop+1);
+
+        leftLength = tempLeft.length;
+        rightLength = tempRight.length;
+
+        int index = 0;
+        int leftCounter = 0;
+        int rightCounter = 0;
+
+        while( (leftCounter < leftLength) && (rightCounter < rightLength)){
+            if(tempLeft[leftCounter] < tempRight[rightCounter]){
+                seq[start+index] = tempLeft[leftCounter];
+                leftCounter++;
+                index++;
+            } else{
+                seq[start+index] = tempRight[rightCounter];
+                rightCounter++;
+                index++;
+            }
+        }
+
+        while(leftCounter < leftLength){
+            seq[start+index] = tempLeft[leftCounter];
+            leftCounter++;
+            index++;
+        }
+
+        while(rightCounter < rightLength){
+            seq[start+index] = tempRight[rightCounter];
+            rightCounter++;
+            index++;
+        }
     }
 }
