1637bbb152a0c883b1c8d5fe6b41ea0d720c4f40
enebert <ebert.eric.n@protonmail.com>
2022-06-13 21:31:09 -0400

    Implemented Binary Search

--- Searching/Search.java
+++ Searching/Search.java
@@ -1,9 +1,28 @@
 public class Search{
-    public index linear_search(int[] seq){
+    public static int linear_search(int[] seq, int value){
+        int length = seq.length;
 
+        for(int i = 0; i < length; i++){
+            if(value == seq[i]) return i;
+        }
+
+        return -1;
     }
 
-    public index binary_search(int[] seq){
-        
+    public static int binary_search(int[] seq, int value){
+        int mid;
+        int first = 0;
+        int last = seq.length-1;
+
+        while(last >= first){
+            mid = (last+first)/2;
+            if(seq[mid] == value) return mid;
+            if(value < seq[mid]){
+                last = mid-1;
+            }else{
+                first = mid+1;
+            }
+        }
+        return -1;
     }
 }
